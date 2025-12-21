d8df0aadfc369acaead63d77beda634b2adcd2a4
jamshid <jamshidalimov3464@gmail.com>
2025-12-21 15:06:28 +0500

    Initial commit

---
+++ app/src/main/java/uz/app/m1lesson31/MainActivity.java
@@ -0,0 +1,35 @@
+package uz.app.m1lesson31;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        textView = findViewById(R.id.tv);
+
+        textView.setOnClickListener(v -> {
+            Animation animation = AnimationUtils.loadAnimation(this, R.anim.rotate);
+            textView.startAnimation(animation);
+        });
+    }
+}
